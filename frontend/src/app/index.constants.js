6e500e5e3bf77df7a6a065e27f3c5970229176e5
jothimoorthy <jothimoorthy.s@gmail.com>
2016-11-22 19:37:52 +0530

    frontend initial structure commit

---
+++ frontend/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('frontResources')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
